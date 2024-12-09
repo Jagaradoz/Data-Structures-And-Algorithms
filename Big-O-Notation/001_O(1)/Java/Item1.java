5fcd645cbb9b4fc693389f33ce1295027653ae80
Jar <jagaradoz@gmail.com>
2024-12-09 22:06:08 +0700

    Add Java O(1) Example code

---
+++ Big-O-Notation/001_O(1)/Java/Item1.java
@@ -0,0 +1,7 @@
+public class Item1 {
+    public static void main(String[] args) {
+        int a = 5;          
+        int b = 10;         
+        int result = a + b; 
+    }
+}
---
+++ Big-O-Notation/001_O(1)/Java/Item2.java
@@ -0,0 +1,6 @@
+public class Item2 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];		
+        int count = arr[0];	
+    }
+}
